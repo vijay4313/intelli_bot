e830922ae16ee7b8279272b187ec7d070752fc60
amrish1222 <amrish1222@gmail.com>
2018-12-16 02:55:47 -0500

    Task 3.20 - Modified controllernode for refactored classes

--- src/controllerNode.cpp
+++ src/controllerNode.cpp
@@ -1,52 +1,45 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018 Venkatraman Narayanan, Amrish Baskaran
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ *
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ *
+ *  @file    controllerNode.cpp
+ *  @Author  Venkatraman Narayanan (vijay4313)
+ *  @Author Amrish Baskaran (amrish1222)
+ *  @copyright  MIT
+ *  @brief  Control Node generator
+ */
+
 #include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "geometry_msgs/Twist.h"
-#include "nav_msgs/Odometry.h"
 #include "../include/Control.h"
-#include "std_msgs/Empty.h"
-#include <sstream>
 
 
 int main(int argc, char **argv) {
 
-  ros::init(argc, argv, "intelli_bot");
-
-  ros::NodeHandle n;
-
-  Control _control;
-
-  _control.setPathPts();
-
-  ros::Subscriber sub = n.subscribe("/ground_truth/state", 1000,
-                                    &Control::navMessageReceived, &_control);
-  ros::Publisher takeOff_msg = n.advertise<std_msgs::Empty>("/ardrone/takeoff",
-                                                            1, true);
-  std_msgs::Empty emptyMsg;
-  takeOff_msg.publish(emptyMsg);
-
-  ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
-
-  ros::Rate loop_rate(10);
-
-  int count = 0;
-  while (ros::ok()) {
-
-    geometry_msgs::Twist new_vel;
-
-    // get Twist msg from control class algorithm
-
-    new_vel = _control.getVelocityPose();
-    ROS_INFO_STREAM(
-        "vel_pub = " << new_vel.linear.x << "," << new_vel.linear.y << ","<< new_vel.linear.z);
-
-    // Publish the computed velocity
-    vel_pub.publish(new_vel);
-
-    ros::spinOnce();
-
-    loop_rate.sleep();
-    ++count;
-  }
-
+  ros::init(argc, argv, "controllerNode");
+  Control ctrl;
+  ros::spin();
   return 0;
 }
