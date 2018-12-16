a4b3cf0969e94025511241052b89d0274501937f
amrish1222 <amrish1222@gmail.com>
2018-12-16 06:56:46 -0500

    Task 3.05 - Created tests for refactored object detector class

--- test/objectDetectorTest.cpp
+++ test/objectDetectorTest.cpp
@@ -26,34 +26,86 @@
  *  @author  Amrish Baskaran(amrish1222)
  *  @copyright	MIT
  *  @date    12/8/2018
- *  @brief	Routine to test ObjectDetector Class
+ *  @brief	objectDetector class test
+ *  @section DESCRIPTION
+ *	A simple testing routine handler
+ *  for the ROS package
+ *
  */
 
 #include "../include/ObjectDetector.h"
 #include <ros/ros.h>
 #include <gtest/gtest.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <intelli_bot/Pedestrians.h>
+#include <intelli_bot/bbox.h>
+#include "../include/ImageProcessor.h"
+#include "sophus/sim3.hpp"
+#include "geometry_msgs/Twist.h"
+#include "visualization_msgs/Marker.h"
+#include "lsd_slam_viewer/keyframeMsg.h"
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/tf.h>
+#include "geometry_msgs/Point.h"
 
-/**
- * @brief Testing function for checking the execution
- *        of the ObjectDetector class
- */
-TEST(TestNodeObjectDetect, executionTest){
-  // waiting for the rosbag to initiate
-	ros::WallDuration(2.0).sleep();
-  // creating the ObjectDetector object
+class point3D {
+public:
+  void point3dCB(const visualization_msgs::Marker points) {
+    markrPtsX = points.points[0].x;
+    markrPtsY = points.points[0].y;
+    markrPtsZ = points.points[0].z;
+  }
+  double markrPtsX;
+  double markrPtsY;
+  double markrPtsZ;
+};
+
+TEST(TestNodeObjectDetect, detectionImageTest) {
+  ros::WallDuration(2.0).sleep();
   ObjectDetector detector;
-  // waiting for the subscriber and publisher initialization
+
+  tf::TransformBroadcaster br;
+  tf::Transform transform;
+  transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
+  tf::Quaternion q;
+  q.setRPY(0, 0, 0);
+  transform.setRotation(q);
+  br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "/nav", "/ardrone_base_frontcam"));
+
   ros::WallDuration(2.0).sleep();
   ros::spinOnce();
-  // retrieve the info on the located pedestrian
-  double x = detector.pedestrians_msg.pedestrians[0].center.x;
-  double y = detector.pedestrians_msg.pedestrians[0].center.y;
-  int width = detector.pedestrians_msg.pedestrians[0].width;
-  int height = detector.pedestrians_msg.pedestrians[0].height;
-  // Testing
+  double x = detector.getPedMsg().pedestrians[0].center.x;
+  double y = detector.getPedMsg().pedestrians[0].center.y;
+  int width = detector.getPedMsg().pedestrians[0].width;
+  int height = detector.getPedMsg().pedestrians[0].height;
   EXPECT_NEAR(185, x, 20.0);
-  EXPECT_NEAR(-72, y, 20.0);
+  EXPECT_NEAR(-72,y, 20.0);
   EXPECT_NEAR(130, width, 20.0);
   EXPECT_NEAR(265, height, 20.0);
- }
+}
 
+TEST(TestNodeObjectDetect, detection3DPointTest) {
+  ros::NodeHandle nh;
+  point3D ptCBHandle;
+  ros::WallDuration(2.0).sleep();
+  ObjectDetector detector;
+
+  tf::TransformBroadcaster br;
+  tf::Transform transform;
+  transform.setOrigin( tf::Vector3(0.0, 0.0, 0.0) );
+  tf::Quaternion q;
+  q.setRPY(0, 0, 0);
+  transform.setRotation(q);
+  br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "/nav", "/ardrone_base_frontcam"));
+
+  ros::WallDuration(2.0).sleep();
+  ros::Subscriber pointSub = nh.subscribe("/humanMarker", 20, &point3D::point3dCB,
+      &ptCBHandle);
+  ros::spinOnce();
+  EXPECT_NEAR(0, ptCBHandle.markrPtsX, 5.0);
+  EXPECT_NEAR(-68,ptCBHandle.markrPtsY, 5.0);
+  EXPECT_NEAR(0, ptCBHandle.markrPtsZ, 5.0);
+}
